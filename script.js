67a33d4c8edd000428e372834db2027c7ee22487
Jarrad Law <jarrad.law@gmail.com>
2016-05-24 00:33:52 +0930

    Filled menu items. Added base images. Added function to toggle visibility of images.

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleLayer(layerID) {
+    var p=document.getElementById(layerID);
+    if(p.className=="active") document.getElementById(layerID).className="";
+    else document.getElementById(layerID).className="active";
+    
+    var img;
+    
+    switch(layerID) {
+        case 'back':
+            img=document.getElementById('backImg');
+            break;
+        case 'obs':
+            img=document.getElementById('obsImg');
+            break;
+        case 'aoi':
+            img=document.getElementById('aoiImg');
+            break;
+        case 'traj':
+            img=document.getElementById('trajImg');
+            break;
+        case 'points':
+            img=document.getElementById('pointsImg');
+            break;
+    }
+    if(img.style.visibility=="visible") img.style.visibility = 'hidden';
+    else img.style.visibility = 'visible';
+}
